9cd54b3e67ef2328c124552af63fa53291a11476
Almohajer00 <m.hamzabaker@gmail.com>
2026-05-05 15:56:41 +0300

    fix: remove cart tables from orders migration file

--- backend/prisma/migrations/20260505122101_add_orders_schema/migration.sql
+++ backend/prisma/migrations/20260505122101_add_orders_schema/migration.sql
@@ -1,25 +1,4 @@
 -- CreateTable
-CREATE TABLE "carts" (
-    "id" SERIAL NOT NULL,
-    "user_id" TEXT NOT NULL,
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMPTZ NOT NULL,
-
-    CONSTRAINT "carts_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "cart_items" (
-    "id" SERIAL NOT NULL,
-    "cart_id" INTEGER NOT NULL,
-    "variant_id" TEXT NOT NULL,
-    "quantity" SMALLINT NOT NULL,
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "cart_items_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "orders" (
     "id" SERIAL NOT NULL,
     "user_id" TEXT NOT NULL,
@@ -51,22 +30,7 @@ CREATE TABLE "order_items" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "carts_user_id_key" ON "carts"("user_id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "cart_items_cart_id_variant_id_key" ON "cart_items"("cart_id", "variant_id");
-
--- CreateIndex
 CREATE UNIQUE INDEX "orders_order_number_key" ON "orders"("order_number");
-
--- AddForeignKey
-ALTER TABLE "carts" ADD CONSTRAINT "carts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "cart_items" ADD CONSTRAINT "cart_items_cart_id_fkey" FOREIGN KEY ("cart_id") REFERENCES "carts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "cart_items" ADD CONSTRAINT "cart_items_variant_id_fkey" FOREIGN KEY ("variant_id") REFERENCES "product_variants"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "orders" ADD CONSTRAINT "orders_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
